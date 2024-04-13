2dcac4905369dbbf44b208b092903ce23d17002e
KassiaRodrigues <kassiarodriguesds@gmail.com>
2024-04-13 19:54:34 -0300

    novos modulos ultima school

--- MOD3S5- Jest/tests/soma.spec.js
+++ MOD3S5- Jest/tests/soma.spec.js
@@ -1,20 +1,20 @@
 // extenção do Jest
 // Teste Correto
-const { sum, sumMelhorado } = require('./soma'); // nome do arquivo JS
+const { sum, sumMelhorado } = require('./../src/soma'); // nome do arquivo JS
 
 it('deve somar 2 + 2 e obter como resultado o valor 4' , () => {
     expect(sum(2,2)).toBe(4) // nome da funçao JS
 });
 
 // Teste Errado 
-it('deve somar 2 + 2 e obter como resultado o valor 4' , () => {
-    expect(sum(2,-2)).toBe(4) // negativo
-});
+// it('deve somar 2 + 2 e obter como resultado o valor 4' , () => {
+//     expect(sum(2,-2)).toBe(4) // negativo
+// });
 
 // Teste Errado 
-it('deve somar 2 + 2 e obter como resultado o valor 4' , () => {
-    expect(sum(2,'2')).toBe(4) // uso de String
-});
+// it('deve somar 2 + 2 e obter como resultado o valor 4' , () => {
+//     expect(sum(2,'2')).toBe(4) // uso de String
+// });
 
 // Teste Correto com validação  
 it('deve somar 2 + 2 e obter como resultado o valor 4' , () => {
--- MOD3S5- Jest/tests/sum.spec.js
+++ MOD3S5- Jest/tests/sum.spec.js
@@ -1,4 +1,4 @@
-const { sum } = require('./soma'); // nome do arquivo JS
+const { sum } = require('./../src/soma'); // nome do arquivo JS
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3); //função JS
